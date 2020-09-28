ed1b4242620f09e7a4d842fd8db0ab92521c153e
RtyXmd <573850993@qq.com>
2020-09-28 17:48:10 +0800

    add namespace

---
+++ ts-web/src/components.ts
@@ -0,0 +1,27 @@
+namespace Components {
+  export namespace SubComponents {
+    export class Test {}
+  }
+
+  export class Header {
+    constructor() {
+      const elem = document.createElement("div");
+      elem.innerText = "This is header";
+      document.body.appendChild(elem);
+    }
+  }
+  export class Content {
+    constructor() {
+      const elem = document.createElement("div");
+      elem.innerText = "This is Content";
+      document.body.appendChild(elem);
+    }
+  }
+  export class Footer {
+    constructor() {
+      const elem = document.createElement("div");
+      elem.innerText = "This is Footer";
+      document.body.appendChild(elem);
+    }
+  }
+}
